d66998a4036cc689cc264bb674eafb506d8d5b85
Nguyễn Phan Anh Tuấn <tuan.nguyenphananh@gmail.com>
2025-01-07 14:44:22 +0700

    Import dotenv

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,20 @@
 import type { NextConfig } from 'next'
+import dotenv from 'dotenv'
+import path from 'node:path'
+import fs from 'node:fs'
+
+const loadEnv = () => {
+    const envPath = 'env'
+
+    dotenv.config({ override: true, path: path.join(envPath, '.env') })
+    dotenv.config({ override: true, path: path.join(envPath, '.env.local') })
+
+    if (!fs.existsSync(path.join(envPath, '.env.local'))) {
+        fs.writeFileSync(path.join(envPath, '.env.local'), '')
+    }
+}
+
+loadEnv()
 
 const nextConfig: NextConfig = {
     /* config options here */
